9218009be249a261e35e0e960fbd097435e19893
vowkaz <jlucasgarmier@gmail.com>
2023-04-16 13:46:41 -0400

    Teste-Creditares

---
+++ src/components/CardCEP.vue
@@ -0,0 +1,227 @@
+<template>
+  <q-card class="my-card rounded-borders"
+          flat bordered
+  >
+    <q-card-section>
+      <article class="q-pa-lg">
+        <div
+          v-if="cep"
+          class="text-h6"
+        >
+          <p>
+            CEP: <label>{{ cep }}</label>
+          </p>
+        </div>
+        <div
+          v-else
+          class="q-mb-lg"
+        >
+          CEP não foi encontrado
+        </div>
+        <p v-if="localidade">
+          {{ localidade }} / {{ uf }}
+        </p>
+        <div v-if="logradouro">
+          {{ logradouro }}, {{ bairro }}
+        </div>
+        <div v-else>
+          Logradouro não foi encontrado
+        </div>
+      </article>
+    </q-card-section>
+    <q-card-actions class="justify-around items-center cols q-ml-auto bg-secondary">
+      <q-btn
+        flat
+        round
+        color="gray"
+        icon="edit"
+        @click="edit = true"
+      />
+      <q-btn
+        flat
+        round
+        color="red"
+        icon="delete"
+        @click="deleteDialog = true"
+      />
+    </q-card-actions>
+    <q-dialog v-model="edit">
+      <q-card>
+        <q-card-section class="row items-center q-pb-none">
+          <div class="text-h6">
+            Edite o endereço
+          </div>
+          <q-space/>
+          <q-btn
+            icon="close"
+            flat
+            round
+            dense
+            v-close-popup
+          />
+        </q-card-section>
+        <q-card-section>
+          <q-form
+            @submit="onSubmit"
+            class="q-gutter-md"
+          >
+            <q-input
+              filled
+              v-model="updateCep"
+              label="Digite seu CEP"
+              mask="#####-###"
+              hint="Cep: 12345/678"
+            />
+            <q-input
+              filled
+              v-model="updateLogradouro"
+              label="Seu logradouro"
+              hint="Av. Fernanda Correa da Costa"
+            />
+            <q-input
+              filled
+              v-model="updateBairro"
+              label="Seu bairro"
+              hint="Jardins"
+            />
+            <div class="row">
+              <q-input
+                filled
+                v-model="updateLocalidade"
+                label="Seu municipio"
+                hint="São Paulo"
+              />
+              <q-input
+                filled
+                v-model="updateUf"
+                label="Sua UF"
+                mask="AA"
+                unmasked-value
+                hint="SP"
+              />
+            </div>
+            <div>
+              <q-btn
+                label="Salvar"
+                type="submit"
+                color="primary"
+                v-close-popup
+              />
+              <q-btn
+                label="Cancelar"
+                color="primary"
+                class="q-ml-sm"
+                outline
+                v-close-popup
+              />
+            </div>
+          </q-form>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+    <q-dialog
+      v-model="deleteDialog"
+      persistent
+    >
+      <q-card>
+        <q-card-section class="row items-center">
+          <q-avatar
+            icon="close"
+            color="negative"
+            text-color="white"
+          />
+          <span class="q-ml-sm">
+            Tem certeza que deseja deletar este endereço?
+          </span>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn
+            flat
+            label="Cancel"
+            color="primary"
+            v-close-popup
+          />
+          <q-btn
+            flat
+            label="Confirmar"
+            color="primary"
+            @click="deleteEvent"
+            v-close-popup
+          />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </q-card>
+</template>
+<script>
+import { ref } from 'vue';
+
+export default {
+  emits: [
+    'delete-element',
+    'update-element',
+  ],
+  methods: {
+    onSubmit() {
+      this.updateEvent();
+      // eslint-disable-next-line no-console
+      console.log(`valor do index = ${this.index}`);
+    },
+    deleteEvent() {
+      this.$emit('delete-element', {
+        index: this.index,
+      });
+    },
+    updateEvent() {
+      this.$emit('update-element', {
+        cep: this.updateCep,
+        bairro: this.updateBairro,
+        localidade: this.updateLocalidade,
+        uf: this.updateUf,
+        logradouro: this.updateLogradouro,
+        index: this.index,
+      });
+    },
+  },
+  data() {
+    return {
+      edit: ref(false),
+      deleteDialog: ref(false),
+      updateIndex: this.index,
+      updateCep: this.cep,
+      updateLogradouro: this.logradouro,
+      updateUf: this.uf,
+      updateLocalidade: this.localidade,
+      updateBairro: this.bairro,
+    };
+  },
+  props: {
+    index: Number,
+    cep: String,
+    uf: String,
+    logradouro: String,
+    localidade: String,
+    bairro: String,
+    setAddress: [],
+  },
+  name: 'CardCEP',
+};
+</script>
+<style scoped>
+.my-card {
+  width: 100%;
+  max-width: 400px;
+}
+
+@media screen and (min-width: 768px) {
+  .my-card {
+    display: flex;
+    flex-direction: row;
+    height: 200px;
+  }
+
+  .cols {
+    flex-direction: column;
+  }
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,250 @@
+<template>
+  <q-page class="flex column">
+    <section class="q-pa-xl row items-center">
+      <q-btn
+        outline
+        align="around"
+        color="green"
+        icon="save"
+        class="q-mx-auto "
+        label="Novo endereço"
+        @click="add = true"
+      />
+      <q-form
+        @submit="findAddress"
+        class="q-gutter-md row items-start q-mx-auto q-pa-lg">
+        <q-input
+          outlined dense
+          debounce="300"
+          v-model="apiCep"
+          placeholder="Procurar"
+          name="apiCep"
+          mask="#####-###"
+          hint="Cep: 12345/678"
+        >
+          <template
+            v-slot:before>
+            <q-icon name="search"/>
+          </template>
+          <template
+            v-slot:append>
+            <q-icon
+              v-if="apiCep !== ''"
+              name="close"
+              @click="clearApiCep"
+            />
+          </template>
+        </q-input>
+        <div>
+          <q-btn label="Submit" type="submit" color="primary"/>
+        </div>
+      </q-form>
+    </section>
+    <section class=" flex row justify-center">
+      <CardCEP
+        class="q-ma-lg"
+        v-for="(address,index) in addresses"
+        :key="address"
+        :index="index"
+        :cep="address.cep"
+        :logradouro="address.logradouro"
+        :bairro="address.bairro"
+        :localidade="address.localidade"
+        :uf="address.uf"
+        @delete-element="deleteEvent"
+        @update-element="updateEvent"
+      />
+    </section>
+    <q-dialog v-model="add">
+      <q-card>
+        <q-card-section class="row items-center q-pb-none">
+          <div class="text-h6">Novo Endereço</div>
+          <q-space/>
+        </q-card-section>
+        <q-card-section>
+          <q-form
+            @submit="onSubmit"
+            @reset="onReset"
+            class="q-gutter-md"
+          >
+            <q-input
+              filled
+              v-model="cep"
+              label="Digite seu CEP"
+              mask="#####-###"
+              hint="Cep: 12345/678"
+            />
+            <q-input
+              filled
+              v-model="logradouro"
+              label="Seu logradouro"
+              hint="Av. Fernanda Correa da Costa"
+            />
+            <q-input
+              filled
+              v-model="bairro"
+              label="Seu bairro"
+              hint="Jardins"
+            />
+            <div class="row">
+              <q-input
+                filled
+                v-model="localidade"
+                label="Seu municipio"
+                hint="São Paulo"
+              />
+              <q-input
+                filled
+                v-model="uf"
+                label="Sua UF"
+                mask="AA"
+                unmasked-value
+                hint="SP"
+              />
+            </div>
+            <div>
+              <q-btn
+                label="Salvar"
+                type="submit"
+                color="primary"
+                v-close-popup
+              />
+              <q-btn
+                label="Cancelar"
+                type="reset"
+                color="primary"
+                class="q-ml-sm"
+                outline
+                v-close-popup
+              />
+            </div>
+          </q-form>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </q-page>
+</template>
+<script>
+import { defineComponent, ref } from 'vue';
+import CardCEP from 'components/CardCEP.vue';
+import { useQuasar } from 'quasar';
+import axios from 'axios';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: { CardCEP },
+  data() {
+    return {
+      cep: '',
+      complemento: '',
+      bairro: '',
+      uf: '',
+      localidade: '',
+      logradouro: '',
+      addresses: [],
+      apiCep: '',
+      add: ref(false),
+      $q: useQuasar(),
+    };
+  },
+  methods: {
+    deleteEvent(address) {
+      this.addresses.splice(address.index, 1);
+      localStorage.setItem('address', JSON.stringify(this.addresses));
+      this.$q.notify({
+        message: 'Endereço removido com êxito.',
+        position: 'top-right',
+        color: 'negative',
+      });
+    },
+    updateEvent(address) {
+      const updatedAddress = {
+        cep: address.cep,
+        bairro: address.bairro,
+        localidade: address.localidade,
+        uf: address.uf.toUpperCase(),
+        logradouro: address.logradouro,
+      };
+      this.addresses.splice(address.index, 1);
+      this.addresses = [...this.addresses, updatedAddress];
+      localStorage.setItem('address', JSON.stringify(this.addresses));
+      this.$q.notify({
+        message: 'Endereço alterado com êxito.',
+        position: 'top-right',
+        color: 'edite',
+      });
+    },
+    onSubmit() {
+      const setAddress = {
+        cep: this.cep,
+        bairro: this.bairro,
+        localidade: this.localidade,
+        uf: this.uf.toUpperCase(),
+        logradouro: this.logradouro,
+      };
+      this.addresses = [...this.addresses, setAddress];
+      localStorage.setItem('address', JSON.stringify(this.addresses));
+      this.onReset();
+      this.$q.notify({
+        message: 'Endereço salvo com êxito.',
+        position: 'top-right',
+        color: 'positive',
+      });
+    },
+    onSubmitAPi() {
+      axios
+        .get(`https://viacep.com.br/ws/${this.apiCep}/json/`)
+        .then((response) => {
+          const apiAdress = {
+            cep: response.data.cep,
+            bairro: response.data.bairro,
+            localidade: response.data.localidade,
+            uf: response.data.uf.toUpperCase(),
+            logradouro: response.data.logradouro,
+          };
+          this.addresses = [...this.addresses, apiAdress];
+          localStorage.setItem('address', JSON.stringify(this.addresses));
+        });
+    },
+    findAddress() {
+      if (this.apiCep === '') {
+        this.addresses = JSON.parse(localStorage.getItem('address'));
+        return;
+      }
+      const index = this.addresses.findIndex((address) => address.cep === this.apiCep);
+      if (index === -1) {
+        this.onSubmitAPi();
+        this.$q.notify({
+          message: 'Endereço não encontrado, gerando um novo!',
+          position: 'top-right',
+          color: 'positive',
+        });
+      } else {
+        this.addresses = [this.addresses[index]];
+        this.$q.notify({
+          message: 'Endereço encontrado com sucesso.',
+          position: 'top-right',
+          color: 'positive',
+        });
+      }
+    },
+    clearApiCep() {
+      this.apiCep = '';
+      this.addresses = JSON.parse(localStorage.getItem('address'));
+    },
+    onReset() {
+      this.cep = '';
+      this.logradouro = '';
+      this.localidade = '';
+      this.uf = '';
+      this.bairro = '';
+    },
+  },
+  mounted() {
+    if (localStorage.address === undefined) {
+      localStorage.address = JSON.stringify([]);
+    }
+    this.addresses = JSON.parse(localStorage.address);
+  },
+});
+</script>
